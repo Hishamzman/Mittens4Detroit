42731ee9591f992e7903409b4cf1f23463934613
MohamedSafaoui <118540741+MohamedSafaoui@users.noreply.github.com>
2023-02-11 12:42:40 -0500

    Add files via upload

---
+++ MittensForDetroit/MittensForDetroit/MittensForDetroitApp.swift
@@ -0,0 +1,20 @@
+//
+//  MittensForDetroitApp.swift
+//  MittensForDetroit
+//
+//  Created by MohamedSafaoui on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MittensForDetroitApp: App {
+    var body: some Scene {
+        WindowGroup {
+           LoadingScreen().environmentObject(TitleTest())
+         //  CloudKitBootcamp()
+            
+            
+        }
+    }
+}
